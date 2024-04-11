cbdfe66725224bfc9d6fdd61478b93ba25b2eadc
dev <caruen@hanmail.net>
2024-04-11 16:44:31 +0900

    feat: docker

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
-pwd
-ls ../
-ls ../../
-cp ../package.json ../package-lock.json ../ecosystem.config.js ../docker/web
+
+cp ../config/.env.production ../package.json ../package-lock.json ../ecosystem.config.js ../docker/web
 cp -r ../src ../docker/web
